ce8bc919b8003baffc26ae7d420554f9fba8d27d
Jie <1503745098@qq.com>
2023-08-20 13:34:21 +0800

    这个提交禁止构建

--- src/main/kotlin/com/bh/planners/core/kether/compat/adyeshach/ActionAdyeshach.kt
+++ src/main/kotlin/com/bh/planners/core/kether/compat/adyeshach/ActionAdyeshach.kt
@@ -3,11 +3,39 @@ package com.bh.planners.core.kether.compat.adyeshach
 import com.bh.planners.api.entity.ProxyAdyeshachEntity
 import com.bh.planners.core.effect.Target
 import com.bh.planners.core.kether.*
-import taboolib.library.kether.ArgTypes
+import com.bh.planners.core.kether.common.CombinationKetherParser
+import com.bh.planners.core.kether.common.ParameterKetherParser
+import ink.ptms.adyeshach.common.entity.EntityTypes
 import taboolib.library.kether.ParsedAction
 import taboolib.module.kether.*
+import java.util.*
 
-object ActionAdyeshach {
+@CombinationKetherParser.Used
+object ActionAdyeshach : ParameterKetherParser("ady", "adyeshach") {
+    val spawn = argumentKetherParser {
+        val reader = this
+        actionNow {
+            run(it).str {
+                val type = EntityTypes.valueOf(it.uppercase(Locale.getDefault()))
+                run(reader.nextParsedAction()).str { name ->
+                    run(reader.nextParsedAction()).long { timer ->
+                        containerOrSender(reader.nextSelectorOrNull()).thenAccept { container ->
+                            val entity = container.filterIsInstance<Target.Location>().map { it.value }
+                            ActionAdyeshachSpawn().apply {
+                                this.type = type
+                                this.name = name
+                                this.timeout = timer
+                                this.selector = entity
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+//object ActionAdyeshach {
 
 
     /**
@@ -19,36 +47,36 @@ object ActionAdyeshach {
      * adyeshach remove [selector]
      *
      */
-    @KetherParser(["adyeshach", "ady"], namespace = NAMESPACE, shared = true)
-    fun parser() = scriptParser {
-        it.switch {
-            case("spawn") {
-                ActionAdyeshachSpawn().apply {
-                    this.type = it.nextParsedAction()
-                    this.name = it.nextParsedAction()
-                    this.timeout = it.nextParsedAction()
-                    this.selector = it.nextSelectorOrNull()
-                }
-            }
-            case("follow") {
-                ActionAdyeshachFollow(
-                    it.nextParsedAction(),
-                    it.nextParsedAction(),
-                    it.nextOptionalAction(arrayOf("option", "params"), "EMPTY")!!
-                )
-            }
-            case("script") {
-                ActionAdyeshachScript(
-                    it.nextParsedAction(),
-                    it.next(ArgTypes.listOf(ArgTypes.ACTION)),
-                    it.nextSelectorOrNull()
-                )
-            }
-            case("remove") {
-                ActionAdyeshachRemove(it.nextSelector())
-            }
-        }
-    }
+//    @KetherParser(["adyeshach", "ady"], namespace = NAMESPACE, shared = true)
+//    fun parser() = scriptParser {
+//        it.switch {
+//            case("spawn") {
+//                ActionAdyeshachSpawn().apply {
+//                    this.type = it.nextParsedAction()
+//                    this.name = it.nextParsedAction()
+//                    this.timeout = it.nextParsedAction()
+//                    this.selector = it.nextSelectorOrNull()
+//                }
+//            }
+//            case("follow") {
+//                ActionAdyeshachFollow(
+//                    it.nextParsedAction(),
+//                    it.nextParsedAction(),
+//                    it.nextOptionalAction(arrayOf("option", "params"), "EMPTY")!!
+//                )
+//            }
+//            case("script") {
+//                ActionAdyeshachScript(
+//                    it.nextParsedAction(),
+//                    it.next(ArgTypes.listOf(ArgTypes.ACTION)),
+//                    it.nextSelectorOrNull()
+//                )
+//            }
+//            case("remove") {
+//                ActionAdyeshachRemove(it.nextSelector())
+//            }
+//        }
+//    }
 
     fun Target.Container.foreachAdyEntity(block: ProxyAdyeshachEntity.() -> Unit) {
         this.forEach<Target.Entity> {
--- src/main/kotlin/com/bh/planners/core/kether/compat/adyeshach/ActionAdyeshachSpawn.kt
+++ src/main/kotlin/com/bh/planners/core/kether/compat/adyeshach/ActionAdyeshachSpawn.kt
@@ -5,26 +5,29 @@ import com.bh.planners.api.entity.ProxyAdyeshachEntity
 import com.bh.planners.api.entity.ProxyEntity
 import com.bh.planners.core.effect.Target
 import com.bh.planners.core.effect.Target.Companion.target
-import com.bh.planners.core.kether.containerOrOrigin
 import com.bh.planners.util.safeSync
 import ink.ptms.adyeshach.api.AdyeshachAPI
 import ink.ptms.adyeshach.common.entity.EntityInstance
 import ink.ptms.adyeshach.common.entity.EntityTypes
 import org.bukkit.Location
-import taboolib.library.kether.ParsedAction
-import taboolib.module.kether.*
-import java.util.*
+import taboolib.module.kether.ScriptAction
+import taboolib.module.kether.ScriptFrame
 import java.util.concurrent.CompletableFuture
 import java.util.function.Consumer
 
 class ActionAdyeshachSpawn : ScriptAction<Target.Container>() {
 
-    lateinit var type: ParsedAction<*>
-    lateinit var name: ParsedAction<*>
-    lateinit var timeout: ParsedAction<*>
-    var selector: ParsedAction<*>? = null
+    lateinit var type: EntityTypes
+    lateinit var name: String
+    var timeout: Long = -1
+    lateinit var selector: List<Location>
 
-    fun spawn(entityType: EntityTypes, locations: List<Location>, name: String, tick: Long): CompletableFuture<List<ProxyEntity>> {
+    fun spawn(
+        entityType: EntityTypes,
+        locations: List<Location>,
+        name: String,
+        tick: Long
+    ): CompletableFuture<List<ProxyEntity>> {
         val future = CompletableFuture<List<ProxyEntity>>()
         spawn(entityType, locations, block = { it.setCustomName(name) }).thenAccept {
             it.forEach { register(it, tick) }
@@ -33,7 +36,11 @@ class ActionAdyeshachSpawn : ScriptAction<Target.Container>() {
         return future
     }
 
-    fun spawn(entityType: EntityTypes, locations: List<Location>, block: Consumer<EntityInstance>): CompletableFuture<List<ProxyAdyeshachEntity>> {
+    fun spawn(
+        entityType: EntityTypes,
+        locations: List<Location>,
+        block: Consumer<EntityInstance>
+    ): CompletableFuture<List<ProxyAdyeshachEntity>> {
         val future = CompletableFuture<List<ProxyAdyeshachEntity>>()
         safeSync {
             future.complete(locations.map { spawn(entityType, it, block) })
@@ -59,23 +66,11 @@ class ActionAdyeshachSpawn : ScriptAction<Target.Container>() {
     override fun run(frame: ScriptFrame): CompletableFuture<Target.Container> {
         val container = Target.Container()
         val future = CompletableFuture<Target.Container>()
-        frame.run(type).str {
-            val type = EntityTypes.valueOf(it.uppercase(Locale.getDefault()))
-            frame.run(name).str { name ->
-                frame.run(timeout).long { timeout ->
-                    frame.containerOrOrigin(selector).thenAccept {
-                        val locations = it.filterIsInstance<Target.Location>().map { it.value }
-                        spawn(type, locations, name, timeout).thenAccept {
-                            container += it.map { it.target() }
-                            future.complete(container)
-                        }
-                    }
-                }
-            }
+        spawn(type, selector, name, timeout).thenAccept {
+            container += it.map { it.target() }
+            future.complete(container)
         }
 
         return future
     }
-
-
 }
