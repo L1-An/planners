2cc6b5fcd53c4394fef7bc2dfc6692d307875db1
_L1An_ <shiyu46@icloud.com>
2023-11-04 17:38:44 +0100

    alpha-142 fix-1
    ✅ 新增Player Damaged Timer，当玩家受到任何伤害时都会触发

---
+++ src/main/kotlin/com/bh/planners/core/timer/bukkit/TPlayerDamaged.kt
@@ -0,0 +1,28 @@
+package com.bh.planners.core.timer.bukkit
+
+import com.bh.planners.core.effect.Target
+import com.bh.planners.core.effect.Target.Companion.toTarget
+import com.bh.planners.core.timer.AbstractTimer
+import com.bh.planners.core.timer.Template
+import org.bukkit.entity.Player
+import org.bukkit.event.entity.EntityDamageEvent
+import taboolib.module.kether.ScriptContext
+
+
+object TPlayerDamaged : AbstractTimer<EntityDamageEvent>() {
+    override val name: String
+        get() = "player damaged"
+    override val eventClazz: Class<EntityDamageEvent>
+        get() = EntityDamageEvent::class.java
+
+    override fun check(e: EntityDamageEvent): Target? {
+        return (e.entity as? Player)?.toTarget()
+    }
+
+    override fun onStart(context: ScriptContext, template: Template, e: EntityDamageEvent) {
+        super.onStart(context, template, e)
+        context.rootFrame().variables()["event"] = e
+        context.rootFrame().variables()["cause"] = e.cause.toString()
+    }
+
+}
